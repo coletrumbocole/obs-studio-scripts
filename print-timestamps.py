c2f21cf853acdec88356cc0622c4d0b26ded6980
Cole <cole.trumbo@gmail.com>
2020-09-19 18:25:08 -0400

    abandoned script

--- print-timestamps.py
+++ print-timestamps.py
@@ -1,16 +1,20 @@
-# C:\Users\colet\Desktop\print-timestamps.py
-# others are in:
-# C:\Program Files\obs-studio\data\obs-plugins\frontend-tools\scripts
-# Python 3.6.8 in C:\Users\colet\AppData\Local\Programs\Python\Python36
+# Quitting this script in favor of using https://github.com/upgradeQ/Scripted-text
 
-import time
-import obspython as obs
+# **********************************************************
+# # C:\Users\colet\Desktop\print-timestamps.py
+# # others are in:
+# # C:\Program Files\obs-studio\data\obs-plugins\frontend-tools\scripts
+# # Python 3.6.8 in C:\Users\colet\AppData\Local\Programs\Python\Python36
 
-# event handlers? 
+# import time
+# import obspython as obs
 
-# how abut just try to run something simple
-print(time.time()) # where can I see logging? 42:50
-# why no log? Wrong Python version, and not pointing to correct folder
-print("heloo wordl")
+# # event handlers? 
 
-obs.script_log(obs.LOG_WARNING, "Error opening URL cole '")
+# # how abut just try to run something simple
+# print(time.time()) # where can I see logging? 42:50
+# # why no log? Wrong Python version, and not pointing to correct folder
+# print("heloo wordl")
+
+# obs.script_log(obs.LOG_WARNING, "Error opening URL cole '")
+
